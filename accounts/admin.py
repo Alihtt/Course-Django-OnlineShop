37cc06ddbc108ca345b6dd359a94a0f0e3157be4
Aliht <aliht.workspace@gmail.com>
2023-12-21 11:23:18 +0330

    custom user model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .forms import UserChangeForm,UserCreationForm
+from .models import User
+from django.contrib.auth.models import Group
 
 # Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+    
+    list_display = ('email','phone_number','is_admin')
+    search_fields = ('email','full_name')
+    filter_horizontal = ()
+    list_filter = ('is_admin',)
+    ordering = ('full_name',)
+    
+    fieldsets = (
+        (None,{'fields':('email','full_name','phone_number','password')}),
+        ('Permissions',{'fields':('is_active','is_admin','last_login')})
+    )
+    add_fieldsets = (
+        (None,{'fields':('phone_number','email','full_name','password1','password2')}),
+    )
+    
+admin.site.unregister(Group)
+admin.site.register(User,UserAdmin)
+    
+    
+    
+    
---
+++ accounts/managers.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import BaseUserManager
+
+class UserManager(BaseUserManager):
+    def create_user(self,phone_number,email,full_name,password):
+        
+        if not phone_number:
+            raise ValueError('You must send phone number')
+        if not email:
+            raise ValueError('You must send email')
+        if not full_name:
+            raise ValueError('You must send full name')
+        
+        user = self.model(phone_number=phone_number,email=email,full_name=full_name)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+        
+    def create_superuser(self,phone_number,email,full_name,password):
+        user = self.create_user(phone_number,email,full_name,password)
+        user.is_admin = True
+        user.save()
+        return user
