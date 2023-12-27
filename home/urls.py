8758e8d7b88d3848d9935d551750361571c5c79e
Aliht <aliht.workspace@gmail.com>
2023-12-27 11:45:30 +0330

    product detail

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name='home'
 urlpatterns = [
-    path('',view=views.HomeView.as_view(),name='home')
+    path('',view=views.HomeView.as_view(),name='home'),
+    path('<slug:slug>/',view=views.ProductDetailView.as_view(),name='product_detail')
 ]
